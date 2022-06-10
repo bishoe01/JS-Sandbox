9645f8c9b9ba6c6f58a64281effdcf6b4ed92327
bishoe01 <bishoe01@gmail.com>
2022-06-10 23:05:53 +0900

    how to hide submit when user input their nickname on it

--- momentum/app.js
+++ momentum/app.js
@@ -5,14 +5,26 @@ const greeting = document.querySelector("#greeting");
 //form하면 submit- 새로고침
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
-    event.preventDefault();
+    event.preventDefault(); //서브밋 후에 새로고침되는 것을 막아줌
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username); //로컬환경에 username을 저장 
-    greeting.innerHTML = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username); //로컬스토리지에 username을 저장 
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerHTML = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+} 
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
