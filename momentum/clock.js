8cc90afc0bb36ee89c2689792e1f08c35d973b22
bishoe01 <bishoe01@gmail.com>
2022-06-21 00:30:33 +0900

    how to implement timeout clock

--- momentum/clock.js
+++ momentum/clock.js
@@ -1,1 +1,15 @@
 const clock = document.querySelector("#clock");
+//Interval 
+
+
+
+function getClock(){
+    const date  = new Date(); //날짜 
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock,1000);
