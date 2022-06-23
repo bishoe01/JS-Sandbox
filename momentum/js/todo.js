1a58b7f7ecba4373f48d52742b53d4ca514582fb
bishoe01 <bishoe01@gmail.com>
2022-06-24 00:11:14 +0900

    make todo list

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,12 +2,25 @@ const toDoForm= document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+const TODOS_KEY = "todos";
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function saveTodos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function paintToDo(newTodo){//list요소를 어떻게 추가해줄 것인가.
     const li = document.createElement("li");
     const span = document.createElement("span");
     const delete_btn = document.createElement("button");
+    delete_btn.innerHTML = "❎";
+    delete_btn.addEventListener("click", deleteToDo)
     li.appendChild(span);
-    span.appendChild(delete_btn);
+    li.appendChild(delete_btn);
     span.innerText = newTodo;
     toDoList.appendChild(li);
 }
@@ -16,6 +29,18 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveTodos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+    console.log("this is turn of", item);
+}
+const savedTodos = localStorage.getItem(TODOS_KEY);
+console.log(savedTodos);
+if(saveTodos){
+    const parsedToDos = JSON.parse(savedTodos);
+    parsedToDos.forEach(sayHello);
+}
